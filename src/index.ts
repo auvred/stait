6eaa075a50149ab7aba55ded1760aa21ef265a95
auvred <aauvred@gmail.com>
2023-11-16 17:46:00 +0000

    test: add tests for actual stait

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { type ActualStaitFromTypestait } from './actual-stait'
 export {
   type Typestait,
   type ActionForTypestait,
---
+++ tests/types/typestait/actual-stait.test.ts
@@ -0,0 +1,44 @@
+import { runTypesTests } from '../../utils'
+
+import type { ActualStaitFromTypestait, Typestait } from '../../../src'
+
+runTypesTests(import.meta.url)
+
+type ExpectType_actual_stait = ActualStaitFromTypestait<
+  Typestait<
+    | {
+        type: 'stait1'
+        context: {
+          data1: string
+          data2: 123
+        }
+        actions: {
+          action1: (payload: number) => 'stait1' | 'stait2'
+        }
+      }
+    | {
+        type: 'stait2'
+        context: {
+          data3: string
+          data4: 123
+        }
+        service: 'stait1' | 'stait2'
+      }
+  >,
+  'stait1'
+>
+
+type ExpectType_multiple_types = ActualStaitFromTypestait<
+  Typestait<
+    | {
+        type: 'stait1'
+      }
+    | {
+        type: 'stait2'
+      }
+    | {
+        type: 'stait3'
+      }
+  >,
+  'stait1' | 'stait2'
+>
